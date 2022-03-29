3a16372fb12ad766f50d9e8a8054ebf5bea26f04
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2022-03-29 09:58:50 +0300

    added files

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const getOriginalFromWords = (listOfWords, text) => {
+    if (listOfWords === undefined || text === undefined && text == "") {
+      return [];
+    }
+    //if (text == "") return [];
+    let compareText = "";
+    let splitedText = text;
+    let final = [];
+    let i = 0;
+    while (i < splitedText.length) {
+      compareText = compareText + text[i];
+      listOfWords.forEach((word) => {
+        if (compareText == word) {
+          final.push(compareText);
+          const splitted = splitedText.split(word)[1];
+          const filteredElments = listOfWords.filter(
+            (element) => element != word
+          );
+          const returnedWords = getOriginalFromWords(filteredElments, splitted);
+          final.push(...returnedWords);
+          i = splitedText.length;
+        }
+      });
+      i++;
+    }
+    return final;
+  };
+  
+  export default {
+    getOriginalFromWords,
+  };
